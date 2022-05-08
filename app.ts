fb3afadf7b08a4f334f098c0e99499df09c7d598
randyhoho <randyhoho2020@gmail.com>
2022-05-08 15:14:38 +0800

    ok

--- app.ts
+++ app.ts
@@ -1,12 +1,13 @@
 import express from 'express';
 import {Request,Response} from 'express';
-import path from 'path';
+// import path from 'path';
 const app = express();
 const PORT = 8080;
 
 app.use('/', express.static('public'))
 app.get('/', function(req:Request, res:Response) {
-    res.redirect(path.join('/jannaandlaw.html'));
+  res.send('hello....');
+    // res.redirect(path.join('/jannaandlaw.html'));
   });
 // http://18.162.84.26/ 3:08pm
 
