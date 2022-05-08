a1b3688746f95056aa73a7f0d3caf45fdaf4580f
randyhoho <randyhoho2020@gmail.com>
2022-05-08 15:08:33 +0800

    3.08

--- app.ts
+++ app.ts
@@ -8,7 +8,7 @@ app.use('/', express.static('public'))
 app.get('/', function(req:Request, res:Response) {
     res.redirect(path.join('/jannaandlaw.html'));
   });
-// http://18.162.84.26/ ok now
+// http://18.162.84.26/ 3:08pm
 
 app.listen(PORT, () => {
     console.log(`Listening at http://localhost:${PORT}/`);
