7079ffe76c56b273348ffee66f79d24eafb1f522
randyhoho <randyhoho2020@gmail.com>
2022-05-08 16:05:40 +0800

    ok

--- app.ts
+++ app.ts
@@ -1,15 +1,18 @@
 import express from 'express';
 import {Request,Response} from 'express';
-// import path from 'path';
+import path from 'path';
 const app = express();
 const PORT = 8080;
 
 app.use('/', express.static('public'))
-app.get('/', function(req:Request, res:Response) {
-  res.send('hello....');
-    // res.redirect(path.join('/jannaandlaw.html'));
+app.get('/hi', function(req:Request, res:Response) {
+    res.redirect(path.join('/jannaandlaw.html'));
   });
 // http://18.162.84.26/ 3:08pm
+
+app.get('*', function(req, res){
+  res.redirect(path.join('/jannaandlaw.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`Listening at http://localhost:${PORT}/`);
