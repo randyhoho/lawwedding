789caa3b1ddd50bd712f5880b2a0d925a01c36d3
randyhoho <randyhoho2020@gmail.com>
2022-05-08 18:28:13 +0800

    add ourpicks album

--- app.ts
+++ app.ts
@@ -5,12 +5,13 @@ const app = express();
 const PORT = 8080;
 
 app.use('/', express.static('public'))
-app.get('/hi', function(req:Request, res:Response) {
-    res.redirect(path.join('/jannaandlaw.html'));
+app.get('/ourpicks', function(req:Request, res:Response) {
+  res.redirect(path.join('/ourpicks.html'));
   });
+  
 // http://18.162.84.26/ 3:08pm
 
-app.get('*', function(req, res){
+app.get('*', function(req:Request, res:Response){
   res.redirect(path.join('/jannaandlaw.html'));
 });
 
